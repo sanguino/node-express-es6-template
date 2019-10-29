914528b07f54ea496cd53784d52f82d0c9e320d4
Miguel G. Sanguino <sanguino@me.com>
2019-10-29 17:23:44 +0100

    feat: added validators, some refactors and test

--- test/app.test.js
+++ test/app.test.js
@@ -30,6 +30,18 @@ describe('Things', () => {
         });
     });
 
+    it('should get a 400 on string id', done => {
+      const id = 'a';
+      chai
+        .request(app)
+        .get(`/${id}`)
+        .end((err, res) => {
+          res.should.have.status(400);
+          res.body.should.be.a('object');
+          done();
+        });
+    });
+
     it('should not get a thing student record', done => {
       const id = 5;
       chai
@@ -37,6 +49,47 @@ describe('Things', () => {
         .get(`/${id}`)
         .end((err, res) => {
           res.should.have.status(404);
+          done();
+        });
+    });
+  });
+
+  describe('POST /', () => {
+    it('should post record', done => {
+      const id = 3;
+      chai
+        .request(app)
+        .post(`/${id}`)
+        .send({ data: 'thing 3' })
+        .end((err, res) => {
+          res.should.have.status(200);
+          res.body.should.be.a('object');
+          done();
+        });
+    });
+
+    it('should get a 400 on string id', done => {
+      const id = 'a';
+      chai
+        .request(app)
+        .post(`/${id}`)
+        .send({ data: 'thing a' })
+        .end((err, res) => {
+          res.should.have.status(400);
+          res.body.should.be.a('object');
+          done();
+        });
+    });
+
+    it('should get a 400 without data in body', done => {
+      const id = 4;
+      chai
+        .request(app)
+        .post(`/${id}`)
+        .send({ bad_name: 'thing a' })
+        .end((err, res) => {
+          res.should.have.status(400);
+          res.body.should.be.a('object');
           done();
         });
     });
